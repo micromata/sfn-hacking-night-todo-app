5663ce443c6779989a789639eb951d20f7f82c3e
René Viering <r.viering@micromata.de>
2016-06-22 16:17:05 +0200

    Integrate jquery and bootstrap js

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,10 @@
+import $ from 'jquery';
+// this is necessary because bootstrap itself checks the existence of jQuery with window.jQuery.
+window.jQuery = $;
+
+// Because of bootstrap is not an UMD Module, we cannot import it with es6 syntax.
+require('bootstrap');
+
 import createRouter from './core/router.js';
 import HomeRoute from './home';
 
